1bec74677adfc79934eada3c28dbbd64e2e008d3
Rikesh-shah <rikeshshah2075@gmail.com>
2025-03-08 07:54:22 +0545

    messages

---
+++ Prompts/messages.py
@@ -0,0 +1,20 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = 'gemini-1.5-pro')
+
+messages = [
+    SystemMessage(content = 'You are a helpful assistant'),
+    HumanMessage(content = "Tell me about LangChain"
+    )
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content = result.content))
+
+print(messages)
+
