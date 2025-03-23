9e640ae8cb89911e85776a4c030bd161ef6f7be6
Rikesh-shah <rikeshshah2075@gmail.com>
2025-03-23 09:45:47 +0545

    lambda runnable

---
+++ Runnables/runnable_lambda.py
@@ -0,0 +1,34 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+
+load_dotenv()
+
+def word_counte(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template = "Generate a joke about {topic}",
+    input_variables = ["topic"]
+)
+
+model = GoogleGenerativeAI(model = "gemini-1.5-pro", temperature = 0.7)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke' : RunnablePassthrough(),
+    'word_count' : RunnableLambda(word_counte)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
