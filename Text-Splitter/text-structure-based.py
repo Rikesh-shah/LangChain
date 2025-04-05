efeb7a3632c7e9ea8ecfbfb59b6876f545f16590
Rikesh-shah <rikeshshah2075@gmail.com>
2025-04-05 22:55:19 +0545

    recursive text splitter

---
+++ Text-Splitter/text-structure-based.py
@@ -0,0 +1,19 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+text = """"
+Yet remarkably appearance get him his projection. Diverted endeavor bed peculiar men the not desirous. Acuteness abilities ask can offending furnished fulfilled sex. Warrant fifteen exposed ye at mistake. Blush since so in noisy still built up an again. As young ye hopes no he place means. Partiality diminution gay yet entreaties admiration. In mr it he mention perhaps attempt pointed suppose. Unknown ye chamber of warrant of norland arrived.
+
+Open know age use whom him than lady was. On lasted uneasy exeter my itself effect spirit. At design he vanity at cousin longer looked ye. Design praise me father an favour. As greatly replied it windows of an minuter behaved passage. Diminution expression reasonable it we he projection acceptance in devonshire. Perpetual it described at he applauded.
+
+To shewing another demands to. Marianne property cheerful informed at striking at. Clothes parlors however by cottage on. In views it or meant drift to. Be concern parlors settled or do shyness address. Remainder northward performed out for moonlight. Yet late add name was rent park from rich. He always do do former he highly.
+"""
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 200,
+    chunk_overlap =0
+)
+
+chunks = splitter.split_text(text)
+
+print(len(chunks))
+print(chunks)
